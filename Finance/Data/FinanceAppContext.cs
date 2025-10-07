99078b7de89ae151031fc255692d5888adadd0db
SofienBenKtib <sofienbenktib11@gmail.com>
2025-10-08 00:17:16 +0100

    initial commit

---
+++ Finance/Data/FinanceAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Finance.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Finance.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Finance/Services/IExpensesService.cs
@@ -0,0 +1,12 @@
+﻿using Finance.Models;
+using System.Collections;
+
+namespace Finance.Services
+{
+    public interface IExpensesService
+    {
+        Task<IEnumerable<Expense>> GetAll();
+        Task Add(Expense expense);
+        IQueryable GetChartData();
+    }
+}
